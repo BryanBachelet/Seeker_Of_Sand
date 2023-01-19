99431723f517edac7dc13e4caf404c445075a203
Bryan Bachelet <bryanbacheletpro@gmailcom>
2023-01-19 21:08:43 +0100

    Enemy Proj

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,15 +11,19 @@ namespace Enemies
         private EnemyManager m_enemyManager;
         private NavMeshAgent m_navAgent;
         private bool m_isDestroy;
+        private Rigidbody m_rigidbody;
+
 
         private void Start()
         {
             m_navAgent = GetComponent<NavMeshAgent>();
+            m_rigidbody = GetComponent<Rigidbody>();
         }
 
         private void Update()
         {
-            m_navAgent.destination = m_target.position;
+            if(m_navAgent.enabled)
+                m_navAgent.destination = m_target.position;
         }
 
         public Enemy(Transform targetTranform, EnemyManager manager)
@@ -43,8 +47,17 @@ namespace Enemies
         {
             return m_isDestroy;
         }
-        public void GetDestroy()
+        public void GetDestroy(Vector3 direction,float power)
         {
+            m_navAgent.enabled = false;
+            float magnitude = Random.Range(0, 1); 
+            m_rigidbody.AddForce(direction.normalized* magnitude * power, ForceMode.Impulse);
+            StartCoroutine(Death());
+        }
+
+        private IEnumerator Death()
+        {
+            yield return new WaitForSeconds(2);
             m_isDestroy = true;
             m_enemyManager.DestroyEnemy(this);
         }
