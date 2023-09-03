2d446ed319b30432ad5b6df73ea9f65d2aede1e9
Bryan Bachelet <bryanbacheletpro@gmailcom>
2023-09-03 17:32:17 +0200

    Add Tuto scene

--- Assets/Scripts/UI/IntroCarrousel.cs
+++ Assets/Scripts/UI/IntroCarrousel.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroCarrousel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Carrousel Parameters")]
+    [SerializeField] private GameObject[] m_panelInfo = new GameObject[9];
+    [SerializeField] private int m_gameplaySceneIndex = 2;
+    private int m_currentIndex;
+
+    public void ChangeScene()
     {
-        
+        SceneManager.LoadScene(m_gameplaySceneIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeUp()
     {
-        
+        m_panelInfo[m_currentIndex].SetActive(false);
+
+        if (m_currentIndex + 1 == m_panelInfo.Length)
+            m_currentIndex = 0;
+        else
+            m_currentIndex++;
+
+        m_panelInfo[m_currentIndex].SetActive(true);
+    }
+
+    public void ChangeDown()
+    {
+        m_panelInfo[m_currentIndex].SetActive(false);
+
+        if (m_currentIndex - 1 == 0)
+            m_currentIndex = m_panelInfo.Length-1;
+        else
+            m_currentIndex--;
+
+        m_panelInfo[m_currentIndex].SetActive(true);
     }
 }
