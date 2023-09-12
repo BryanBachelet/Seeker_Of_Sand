e25e44f4512dd23a154d5a87e51975ad6e1dfc4d
Guilleade <titoug56@gmail.com>
2023-09-12 16:43:58 +0200

    reglage light sur les autels

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -185,7 +185,7 @@ namespace Render.Camera
         {
             if (ctx.performed && !m_mouseInputActivate)
             {
-
+                
                 float value = ctx.ReadValue<float>();
                 if (m_inverseCameraController) value = -1 * value;
                 m_signValue = value;
@@ -233,12 +233,16 @@ namespace Render.Camera
         {
             if (ctx.performed && m_mouseInputActivate)
             {
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = false;
                 m_isRotationInputPress = true;
 
             }
             if (ctx.canceled && m_mouseInputActivate)
             {
                 m_isRotationInputPress = false;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
             }
         }
 
