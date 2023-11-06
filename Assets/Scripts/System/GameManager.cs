4f8712b172082374f0e68604c9c82134d9e76841
Bryan Bachelet <bryanbacheletpro@gmailcom>
2023-11-06 15:36:28 +0100

    Add Save of end game data + base profile

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public string profileName;
+    // Start is called before the first  frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject); 
+    }
+
+
+}
