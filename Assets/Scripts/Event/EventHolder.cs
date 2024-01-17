c84687f62a9099c2be012ad800cc5fb30148eda6
Guilleade <titoug56@gmail.com>
2024-01-17 16:23:47 +0100

    Add bird

--- Assets/Scripts/Event/EventHolder.cs
+++ Assets/Scripts/Event/EventHolder.cs
@@ -5,30 +5,47 @@ using UnityEngine;
 public class EventHolder : MonoBehaviour
 {
     public List<AltarBehaviorComponent> altarBehaviorList = new List<AltarBehaviorComponent>();
+    static public Vector3[] altarTransformTab;
+    public Vector3[] altarTransformTabDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetAltar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public List<AltarBehaviorComponent> GetAltar()
     {
         altarBehaviorList.Clear();
-        for(int i = 0; i < transform.childCount; i++)
+        int childCount = transform.childCount;
+        Vector3[] newAltarTransform = new Vector3[childCount];
+        for(int i = 0; i < childCount; i++)
         {
             if (transform.GetChild(i).GetComponent<AltarBehaviorComponent>())
             {
                 altarBehaviorList.Add(transform.GetChild(i).GetComponent<AltarBehaviorComponent>());
+                newAltarTransform[i] = transform.GetChild(i).transform.position;
             }
         }
+        altarTransformTab = newAltarTransform;
+        altarTransformTabDisplay = altarTransformTab;
         return altarBehaviorList;
     }
 
-
+    static public Vector4 newSpot(int currentSpot)
+    {
+        int newSpotNumber = currentSpot;
+        while (newSpotNumber == currentSpot)
+        {
+            newSpotNumber = Random.Range(0, altarTransformTab.Length);
+        }
+        Vector4 nextAltarPosition = altarTransformTab[newSpotNumber];
+        nextAltarPosition.w = newSpotNumber;
+        return nextAltarPosition;
+    }
 }
