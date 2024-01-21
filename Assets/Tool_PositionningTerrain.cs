497fc5fa6f2197ab716f10648bd86b1ec0c7d0b8
Guilleade <titoug56@gmail.com>
2024-01-21 15:06:43 +0100

    Add Bridge prefab

---
+++ Assets/Tool_PositionningTerrain.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Tool_PositionningTerrain : MonoBehaviour
+{
+    public static bool activeDebug;
+    public bool activeDebugDisplay;
+
+    public LayerMask layerGround;
+    public Vector3 directionPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeDebugDisplay)
+        {
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(directionPosition), out hit, Mathf.Infinity, layerGround))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(directionPosition) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                transform.position = hit.point;
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(directionPosition) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+
+        }
+        Debug.DrawRay(transform.position, transform.TransformDirection(directionPosition) * 1000, Color.white);
+        Debug.Log("Did not Hit");
+    }
+}
