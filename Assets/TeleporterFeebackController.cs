fe77d30ff671426a979afeb82a07e21085eed6a4
Guilleade <titoug56@gmail.com>
2024-05-26 14:47:11 +0200

    Set reward sign

--- Assets/TeleporterFeebackController.cs
+++ Assets/TeleporterFeebackController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class TeleporterFeebackController : MonoBehaviour
 {
@@ -10,15 +11,42 @@ public class TeleporterFeebackController : MonoBehaviour
     public Color[] colorSymboleDecal = new Color[4];
     public Texture[] textureReward = new Texture[4];
 
+    [Range(0, 6)]
+    public int rewardToUse = 0;
+    public MeshRenderer socleMesh;
+    public Material socleSpawn_mat;
+    public Material dissonance_mat;
+
+    public VisualEffect zoneAutourVFX;
+    public MeshRenderer planeSymboleEffect;
+    public Material planeSymbole_mat;
+
+    public bool activeChange = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(socleMesh)
+        {
+            socleSpawn_mat = socleMesh.materials[0];
+            dissonance_mat = socleMesh.materials[1];
+            planeSymbole_mat = planeSymboleEffect.material;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(activeChange)
+        {
+            activeChange = false;
+            ChangeRewardID(rewardToUse);
+        }
         
+    }
+
+    public void ChangeRewardID(int ID)
+    {
+        socleSpawn_mat.SetTexture("_MaskSelfLit", textureReward[ID]);
+        planeSymbole_mat.SetTexture("_Symbole", textureReward[ID]);
     }
 }
