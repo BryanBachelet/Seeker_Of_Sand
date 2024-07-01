7cf742ee9bc08ef219df9e5e0b7c8410c11e7900
Guilleade <titoug56@gmail.com>
2024-07-01 13:06:37 +0200

    Add Masse script for sound design

---
+++ Assets/Sound_Masse_Behavior.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class Sound_Masse_Behavior : MonoBehaviour
+{
+    public int typeEnemyObserved = 0;
+    public string variableFMOD;
+    public int[] seuil = new int[5];
+    public int currentSeuil = 0;
+    public EventReference masseSoundAssociated;
+    public EventInstance masseSoundInstance;
+
+    public Transform player;
+    private Vector3 playerPos;
+
+    public LayerMask enemiLayer;
+    public int rangeDetection;
+    private Collider[] nearEnemi;
+    public int nearEnemiCount = 0;
+
+    public float cooldownDetection = 1;
+    private float lastTimeDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player == null) { player = GameObject.Find("Player").transform; }
+
+        masseSoundInstance = RuntimeManager.CreateInstance(masseSoundAssociated);
+        masseSoundInstance.start();
+        masseSoundInstance.setVolume(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > lastTimeDetection + cooldownDetection)
+        {
+            CountNearEnnemi();
+        }
+        //transform.position = player.transform.position;
+    }
+
+    public void CountNearEnnemi()
+    {
+        int tempEnemiCount = 0;
+        Vector3 positionMoyenne = Vector3.zero;
+        playerPos = player.transform.position;
+        nearEnemi = Physics.OverlapSphere(player.position,rangeDetection,enemiLayer);
+        //nearEnemiCount = nearEnemi.Length;
+        lastTimeDetection = Time.time;
+        foreach (Collider item in nearEnemi)
+        {
+            if (item.GetComponent<Enemies.NpcMetaInfos>())
+            {
+                Enemies.NpcMetaInfos metaObserved = item.GetComponent<Enemies.NpcMetaInfos>();
+                if((int)metaObserved.type == typeEnemyObserved)
+                {
+                    Debug.Log("EnemiAdd");
+                    tempEnemiCount++;
+                    positionMoyenne += metaObserved.transform.position;
+                }
+                else
+                {
+                    Debug.Log("EnemiNotAdd");
+                }
+            }
+             
+        }
+        if(tempEnemiCount != nearEnemiCount) { nearEnemiCount = tempEnemiCount; }
+        if (nearEnemiCount > 0)
+        {
+            for(int i = 0; i < seuil.Length; i++)
+            {
+                if(i == 0)
+                {
+                    if(nearEnemiCount > 0 && nearEnemiCount < seuil[i+1])
+                    {
+                        currentSeuil = i - 1;
+                    }
+                }
+                else
+                {
+                    if(i < seuil.Length - 1)
+                    {
+                        if (nearEnemiCount > seuil[i] && nearEnemiCount < seuil[i + 1])
+                        {
+                            currentSeuil = i - 1;
+                        }
+                    }
+                    else
+                    {
+                        if (nearEnemiCount >= seuil[i])
+                        {
+                            currentSeuil = i - 1;
+                        }
+                    }
+                }
+            }
+            positionMoyenne /= nearEnemiCount;
+            transform.position = positionMoyenne;
+            transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+        }
+        else
+        {
+            currentSeuil = 0;
+        }
+        masseSoundInstance.setParameterByName(variableFMOD, currentSeuil);
+        masseSoundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(transform.position));
+        Debug.Log("Send Seuil Masse : [" + currentSeuil + "]");
+    }
+}
