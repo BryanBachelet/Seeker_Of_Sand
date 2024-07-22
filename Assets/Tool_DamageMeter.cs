7130e18c9fa8f1c2f9f85caf2686947ae41c51f0
Bryan Bachelet <bryanbacheletpro@gmail.com>
2024-07-22 11:24:00 +0200

    Fix an error in tool damage meter

--- Assets/Tool_DamageMeter.cs
+++ Assets/Tool_DamageMeter.cs
@@ -13,7 +13,7 @@ public class Tool_DamageMeter : MonoBehaviour
     private float time;
     private float lastTimeCheck;
     // Start is called before the first frame update
-    void Start()r
+    void Start()
     {
         dps = 0;
         dpsCumule = 0;
