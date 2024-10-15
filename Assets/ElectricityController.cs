6489e14ea5509b4ecc74f2bbff82812bc166c171
Titouan <titoug56@gmail.com>
2024-10-15 14:12:48 +0200

    Change TS border

--- Assets/ElectricityController.cs
+++ Assets/ElectricityController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ElectricityController : MonoBehaviour
 {
     public Transform[] elecPosition = new Transform[4];
