e8bce27196a59fe7b7fe2cfca535181c92b46507
Bryan Bachelet <bryanbacheletpro@gmail.com>
2024-11-15 16:03:45 +0100

    [Feat] Add Stat on Spell Choice UI

--- Assets/Scripts/Character/Upgrade/UpgradeManager.cs
+++ Assets/Scripts/Character/Upgrade/UpgradeManager.cs
@@ -280,6 +280,7 @@ public class UpgradeManager : MonoBehaviour
         if (book_Animator != null) book_Animator.SetBool("BookOpen", false);
         float time = Time.time;
         float timeToClose = book_Animator.GetCurrentAnimatorStateInfo(0).length;
+        m_chooseSpellManagerComponent.CloseSpellChoiceUI();
 
         StartCoroutine(CloseUIWithDelay(timeToClose));
 
