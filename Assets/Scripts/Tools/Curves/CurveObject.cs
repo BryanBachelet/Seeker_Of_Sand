9683e9e514d1bfa7c8862a5c9490f0c837c06146
Bryan Bachelet <bryanbacheletpro@gmail.com>
2025-04-16 13:38:30 +0200

    [Feat] Curve Tool update

--- Assets/Scripts/Tools/Curves/CurveObject.cs
+++ Assets/Scripts/Tools/Curves/CurveObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using ExcelLibrary.SpreadSheet;
+using UnityEngine.UIElements;
 namespace GuerhoubaTools.Curves
 {
     [CreateAssetMenu(fileName = "Curve Object", menuName = "Tools Guerhouba", order = 0)]
@@ -16,7 +16,24 @@ namespace GuerhoubaTools.Curves
 
 #if UNITY_EDITOR
 
-        [SerializeField] public Object csvfile;
+        [SerializeField] private Object csvfile;
+
+        [SerializeField] private bool m_maxLength;
+        [SerializeField] private int m_lineTimeIndex=0;
+        [SerializeField] private int m_columnTimeStart=0;
+
+        [SerializeField] private int m_lineValueIndex = 0;
+        [SerializeField] private int m_columnValueStart = 0;
+
+        [SerializeField] private int m_dataRangeSize = 0;
+
+
+
+        // Stop column length when null;
+        // Check if line index is valid 
+        // Check column length is valid 
+        // Check column start is valid
+
         public void SetCurve()
         {
             string path =AssetDatabase.GetAssetPath(csvfile);
@@ -30,15 +47,63 @@ namespace GuerhoubaTools.Curves
 
             string allText = System.IO.File.ReadAllText( path);
             string[] lines = allText.Split('\n');
-            Debug.Log("The lines count  " + lines.Length);
-            for (int i = 0; i < lines.Length; i++)
+            int linesCount =  lines.Length;
+            int cellsMaxCount = lines[1].Split(',').Length;
+
+            if (!IsCSVIndexValid(linesCount,cellsMaxCount)) return;
+
+            AnimationCurve tmpAnimationCurve = m_curve;
+
+            string[] cellsTime = lines[m_lineTimeIndex].Split(',');
+            string[] cellsValue = lines[m_lineValueIndex].Split(',');
+
+
+            tmpAnimationCurve.ClearKeys();
+            for (int i = 0; i < m_dataRangeSize; i++)
             {
-                string[] cells1 = lines[1].Split(',');
-                Debug.Log("The cells " +i+" count  " + cells1.Length);
+                string valueStr = cellsValue[m_columnValueStart + i].Trim('\"');
+                string timeValueStr = cellsTime[m_columnTimeStart + i].Trim('\"');
+
+                float timeValue =0;
+                float ordoValue =0;
+                bool isTimeValid = float.TryParse(timeValueStr,out timeValue);
+                bool isOrdoValid = float.TryParse(valueStr,out ordoValue);
+                if (!isTimeValid || !isOrdoValid)
+                {
+                    if (!isTimeValid) Debug.LogError("Cant parse time data in the index " + (m_columnTimeStart + i));
+                    if (!isOrdoValid) Debug.LogError("Cant parse value data int the index " + (m_columnValueStart + i));
+                    return;
+                }
+                   
+                tmpAnimationCurve.AddKey(timeValue, ordoValue);
             }
+
+            m_curve = tmpAnimationCurve; 
+
+        }
+
+        private bool IsCSVIndexValid(int lineMaxValue, int columnMaxValue)
+        {
+
+
+            if (!IsValidIndex("Line Value",m_lineValueIndex, lineMaxValue)) return false;
+            if (!IsValidIndex("Line Time", m_lineTimeIndex, lineMaxValue)) return false;
+            if (!IsValidIndex("Column Start Value",m_columnValueStart,columnMaxValue)) return false;
+            if (!IsValidIndex("Column Start Time", m_columnTimeStart, columnMaxValue)) return false;
+            if (!IsValidIndex("Column Length ", m_dataRangeSize, columnMaxValue)) return false;
+
+            return true;
+        }
+
+        private bool IsValidIndex(string nameVariable, int value, int maxValue)
+        {
+            if (value < 0 || value >= maxValue )
+            {
+                Debug.LogError("The " + nameVariable + " value is not valid. The max line value is " + maxValue);
             
-
-
+                return false;
+            }
+            return true;
         }
 #endif
 
