d4e4b84244669926fd7907259be08ca7a64f9bde
Bryan Bachelet <bryanbacheletpro@gmail.com>
2025-08-13 10:03:09 +0200

    [Feat] Start Dissonance heart

---
+++ Assets/Scripts/Rooms/DissonanceHeartBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissonanceHeartBehavior : InteractionInterface
+{
+    public override void OnInteractionEnd(GameObject player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnInteractionStart(GameObject player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
